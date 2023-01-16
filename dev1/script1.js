f4b5a497dacfd7ce7fc5f34073c048a389cf364a
Shohan20lac <63243156+Shohan20lac@users.noreply.github.com>
2023-01-16 21:10:46 +0600

    rock paper scissor session1
    started putting headings

---
+++ dev1/script1.js
@@ -0,0 +1,3 @@
+const computerChoice = document.getElementById('computer_choice');
+const playerChoice = document.getElementById('player_choice');
+const resultDisplay = document.getElementById('result');
