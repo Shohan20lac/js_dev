986d4decdadd05929b0bb806be5c35440c2e96a9
Shohan20lac <63243156+Shohan20lac@users.noreply.github.com>
2023-01-17 11:41:32 +0600

    Update script1.js

--- dev1/script1.js
+++ dev1/script1.js
@@ -1,3 +1,6 @@
-const computerChoice = document.getElementById('computer_choice');
-const playerChoice = document.getElementById('player_choice');
-const resultDisplay = document.getElementById('result');
+const computerChoice = document.getElementById('computer_choice')
+const playerChoice = document.getElementById('player_choice')
+const resultDisplay = document.getElementById('result')
+
+// detect all elements that are button types
+const possibleChoices = document.querySelectorAll('button')
